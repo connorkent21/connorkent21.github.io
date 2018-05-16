de8acaf1b7ed359cf10b3b3c2cdb77fbb08d659b
connorkent21 <30322214+connorkent21@users.noreply.github.com>
2018-05-16 19:13:16 -0400

    jquery update & mobile fix

--- home.js
+++ home.js
@@ -1,13 +1,35 @@
-var backToTop = document.getElementById("btt");
+// var backToTop = document.getElementById("btt");
 
-backToTop.addEventListener("click", function(){
-	window.location.href = "#top";
+// backToTop.addEventListener("click", function(){
+// 	window.location.href = "#top";
+// });
+
+// backToTop.addEventListener("mouseover", function(){
+// 	this.classList.add("hovered");
+// });
+
+// backToTop.addEventListener("mouseout", function(){
+// 	this.classList.remove("hovered");
+// });
+
+window.onload=function() {
+	$("#content").toggleClass("hidden");
+	$("#content").hide().fadeIn(1000); 
+} 
+
+
+$("#btt").on("click", function(){
+	$("html").animate({scrollTop: 0}, "slow");
 });
 
-backToTop.addEventListener("mouseover", function(){
-	this.classList.add("hovered");
+$("#aboutLink").on("click", function(){
+	$("html").animate({scrollTop: $("#about").offset().top}, "slow");
 });
 
-backToTop.addEventListener("mouseout", function(){
-	this.classList.remove("hovered");
+$("#projectsLink").on("click", function(){
+	$("html").animate({scrollTop: $("#projects").offset().top}, "slow");
+});
+
+$("#projectsBtn").on("click", function(){
+	$("html").animate({scrollTop: $("#projects").offset().top}, "slow");
 });
